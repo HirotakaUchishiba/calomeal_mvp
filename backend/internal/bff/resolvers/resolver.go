04516bd756fbad20ecc36e26013ae0c0fd41e530
Hirotaka <hirotaka@HirotakanoMacBook-Air.local>
2025-09-03 14:20:27 +0900

    resolver.goのResolver構造体に新しいサービスを追加

--- backend/internal/bff/resolvers/resolver.go
+++ backend/internal/bff/resolvers/resolver.go
@@ -1,12 +1,18 @@
 // backend/internal/bff/resolvers/resolver.go
 package resolvers
 
-import "github.com/HirotakaUchishiba/calomeal_mvp/backend/internal/service/user" // この行を追加
+import (
+	"github.com/HirotakaUchishiba/calomeal_mvp/backend/internal/service/fooddata" // 追加
+	"github.com/HirotakaUchishiba/calomeal_mvp/backend/internal/service/log"      // 追加
+	"github.com/HirotakaUchishiba/calomeal_mvp/backend/internal/service/user"
+)
 
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
 type Resolver struct {
-	UserService user.Service // この行を追加
+	UserService     user.Service
+	FoodDataService fooddata.Service // 追加
+	LogService      log.Service      // 追加
 }
